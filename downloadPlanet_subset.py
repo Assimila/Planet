8e890c3f04bf0f60fd384ee3b0b5986ff2a755f5
= <taylor.day@assimila.eu>
2018-02-05 10:46:11 +0000

    Reading AOI from geojson and parallelising requests

--- downloadPlanet_subset.py
+++ downloadPlanet_subset.py
@@ -34,72 +34,29 @@ import os
 import requests
 import time
 import osgeo.gdal as gdal
+import json
+from multiprocessing.dummy import Pool as ThreadPool
 
 from planet import api
 from planet.api import filters
 from sys import stdout
 
-# GeoJSON AOI
-# e.g. http://geojson.io
-aoi = {
-  "type": "Polygon",
-  "coordinates": [
-    [
-      [-122.45593070983887, 37.76060492968732],
-      [-122.41996765136719, 37.76060492968732],
-      [-122.41996765136719, 37.80184969073113],
-      [-122.45593070983887, 37.80184969073113],
-      [-122.45593070983887, 37.76060492968732]
-    ]
-  ]
-}
+def activate_item(item_info):
 
-# Get API key
-api_key = api.ClientV1().login( 'lopez.saldana@gmail.com', 'gertan20' )
+    """
+    This is the main method that activates and downloads the API
+    request. It is set up to handle parallelism so that multiple
+    requests can be handled at once using ThreadPool.
 
-# Create client
-client = api.ClientV1( api_key = api_key['api_key'] )
+    :param item_info: This is the line of the item_info text file
+                      which details the item_id and item_type.
 
-# Build a query using the AOI and a cloud_cover filter
-# that get images with lower than 10% cloud cover
-# and acquired on Nov 1st, 2017
-query = filters.and_filter(
-    filters.geom_filter( aoi ),
-    filters.range_filter( 'cloud_cover', lt = 0.1 ),
-    filters.date_range( 'acquired', 
-                        gte = '2017-11-01T00:00:00.000Z',
-                        lte = '2017-11-01T23:59:00.000Z' )
-)
+    :return: N/A
+    """
 
-# Build a request for only PlanetScope imagery
-# Item types:
-#   https://www.planet.com/docs/reference/data-api/items-assets
-request = filters.build_search_request(
-    query, item_types = [ 'PSScene4Band' ]
-)
-
-# Get results
-result = client.quick_search(request)
-
-# Setup auth
-session = requests.Session()
-session.auth = ( api_key['api_key'], '')
-
-# Set asset type to download
-asset_type = 'analytic'
-
-stdout.write('item_id,item_type,cloud_cover,date\n')
-
-# items_iter returns a limited iterator of all results,
-# behind the scenes, the client is paging responses from the API
-NumberOfImages = 20
-for item in result.items_iter( limit = NumberOfImages ):
-    props = item['properties']
-    item_id = item['id']
-    item_type = item['properties']['item_type']
-
-    stdout.write('{0},{item_type},{cloud_cover},' \
-                 '{acquired}\n'.format(item['id'], **props))
+    # Strip out the item_type and item_id from item_info
+    item_id, item_type = item_info.split(' ', 2)
+    stdout.write("attempting to obtain "+item_id+item_type)
 
     # Request an item
     item_to_download = \
@@ -112,7 +69,7 @@ for item in result.items_iter( limit = NumberOfImages ):
                           asset_type]["_links"]["activate"]
 
     # Request activation
-    response = session.post( item_activation_url )
+    response = session.post(item_activation_url)
 
     # HTTP 204: Success, No Content to show
     while response.status_code <> 204:
@@ -120,7 +77,7 @@ for item in result.items_iter( limit = NumberOfImages ):
         print "Waiting for activation code..."
         # Activation will take ~8 minutes. Run the command above again
         # until you see a URL in the 'location' element of the response.
-        time.sleep( 9 * 60 )
+        time.sleep(9*60)
         # Request an item
         item_to_download = \
             session.get(
@@ -132,7 +89,7 @@ for item in result.items_iter( limit = NumberOfImages ):
                                   asset_type]["_links"]["activate"]
 
         # Request activation once again...
-        response = session.post( item_activation_url )
+        response = session.post(item_activation_url)
 
     # Get location of the asset
     asset_location_url = item_to_download.json()[asset_type]['location']
@@ -141,12 +98,158 @@ for item in result.items_iter( limit = NumberOfImages ):
     vsicurl_url = '/vsicurl/' + asset_location_url
     output_file = item_id + '_subarea.tif'
 
-    # TODO
-    # Get angular information from metadata
-    # -- sensor and sun angles --
-
     # GDAL Warp crops the image by our AOI, and saves it
     subset_fname = 'subset.geojson'
-    gdal.Warp( output_file, vsicurl_url, dstSRS = 'EPSG:4326', 
-               cutlineDSName = subset_fname, cropToCutline = True )
+    gdal.Warp(output_file, vsicurl_url, dstSRS='EPSG:4326',
+               cutlineDSName=subset_fname, cropToCutline=True)
+
+# ========================================================================== #
+
+# GeoJSON AOI
+# e.g. http://geojson.io
+# Reads the subset.geojson file within the planet directory
+with open("subset.geojson") as f:
+    geoj = json.load(f)
+
+aoi = {
+     "type": geoj['features'][0]['geometry']['type'],
+     "coordinates": geoj['features'][0]['geometry']['coordinates']
+}
+
+# Get API key
+api_key = api.ClientV1().login('lopez.saldana@gmail.com', 'gertan20')
+
+# Create client
+client = api.ClientV1(api_key=api_key['api_key'])
+
+# Build a query using the AOI and a cloud_cover filter
+# that get images with lower than 10% cloud cover
+# and acquired on Nov 1st, 2017
+query = filters.and_filter(
+    filters.geom_filter(aoi),
+    filters.range_filter('cloud_cover', lt=0.1),
+    filters.date_range('acquired',
+                        gte='2017-03-01T00:00:00.000Z',
+                        lte='2017-03-05T23:59:00.000Z')
+)
+
+# Build a request for only PlanetScope imagery
+# Item types:
+#   https://www.planet.com/docs/reference/data-api/items-assets
+request = filters.build_search_request(
+    query, item_types=['PSScene4Band'])
+
+# Get results
+result = client.quick_search(request)
+
+# Setup auth
+session = requests.Session()
+session.auth = (api_key['api_key'], '')
+
+# Set asset type to download
+asset_type = 'analytic'
+
+stdout.write('item_id,  item_type,  cloud_cover,  date\n')
+
+# Set a limit for the maximum number of results to download from the API
+# that have been returned from the API request.
+NumberOfImages = 200
+
+# Loop over each item found during the request and append the info
+# to an image_ids text file so that it can be passed to the ThreadPool
+# to parallel-ize the requests.
+#
+# Also, save the metadata of each file to a text file with the filename
+# set to the image_id.
+with open("image_ids.txt", 'w') as id_file:
+
+    # items_iter returns a limited iterator of all results,
+    # behind the scenes, the client is paging responses from the API
+    for item in result.items_iter(limit=NumberOfImages):
+        props = item['properties']
+        item_id = item['id']
+        item_type = item['properties']['item_type']
+
+        stdout.write('{0}  ,{item_type},  {cloud_cover},  '
+                     '{acquired}\n'.format(item['id'], **props))
+
+        # Update T. Day 05/02/2018
+        # Save property dictionary to txt file to
+        # obtain and save angular information from metadata
+        # as gdal.Warp removes this information.
+
+        with open(str(item_id)+".txt", 'w') as metadata_file:
+            metadata_file.write(json.dumps(props))
+
+        # Append each item_id to a text file to support parallelism
+        id_file.write(item_id+" "+item_type+"\n")
+
+# Reopen the image_id text file in read mode to
+# pass to the activate_item
+with open("image_ids.txt", 'r') as id_file:
+    item_info = id_file.read().splitlines()[:]
+
+    # Set up the parallelism so that 5 requests can run simultaneously
+    parallelism = 5
+    thread_pool = ThreadPool(parallelism)
+
+    # All items will be sent to the 'activate_item' function but only
+    # 5 will be run at once
+    thread_pool.map(activate_item, item_info)
+
+# ================================================================= #
+
+
+#     # Request an item
+#     item_to_download = \
+#         session.get(
+#         ("https://api.planet.com/data/v1/item-types/" +
+#         "{}/items/{}/assets/").format(item_type, item_id))
+#
+#     # Extract the activation url from the item for the desired asset
+#     item_activation_url = item_to_download.json()[
+#                           asset_type]["_links"]["activate"]
+#
+#     # Request activation
+#     response = session.post(item_activation_url)
+#
+#     # HTTP 204: Success, No Content to show
+#     while response.status_code <> 204:
+#         print "Response code:", response.status_code
+#         print "Waiting for activation code..."
+#         # Activation will take ~8 minutes. Run the command above again
+#         # until you see a URL in the 'location' element of the response.
+#         time.sleep( 9 * 60 )
+#         # Request an item
+#         item_to_download = \
+#             session.get(
+#                 ("https://api.planet.com/data/v1/item-types/" +
+#                  "{}/items/{}/assets/").format(item_type, item_id))
+#
+#         # Extract the activation url from the item for the desired asset
+#         item_activation_url = item_to_download.json()[
+#                                   asset_type]["_links"]["activate"]
+#
+#         # Request activation once again...
+#         response = session.post(item_activation_url)
+#
+#     # Get location of the asset
+#     asset_location_url = item_to_download.json()[asset_type]['location']
+#
+#     # Subset
+#     vsicurl_url = '/vsicurl/' + asset_location_url
+#     output_file = item_id + '_subarea.tif'
+#
+    # Update T. Day 05/02/2018
+    # Save property dictionary to txt file to
+    # obtain and save angular information from metadata
+    # as gdal.Warp removes this information.
+
+    #with open(str(item_id) + ".txt", 'w') as file:
+    #    file.write(json.dumps(props))
+#
+#     # GDAL Warp crops the image by our AOI, and saves it
+#     subset_fname = 'subset.geojson'
+#     gdal.Warp( output_file, vsicurl_url, dstSRS = 'EPSG:4326',
+#                cutlineDSName = subset_fname, cropToCutline = True )
 
